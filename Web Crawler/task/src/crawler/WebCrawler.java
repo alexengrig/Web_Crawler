5eb90da1efab3f9d782ce250bebb9032d5b31b54
Grig Alex <alexengrigdev@gmail.com>
2020-02-16 17:17:32 +0300

    Add 1/6 stage solution

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -6,6 +6,15 @@ public class WebCrawler extends JFrame {
     public WebCrawler() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+        setTitle("Simple Window");
+        initComponents();
         setVisible(true);
+    }
+
+    private void initComponents() {
+        JTextArea textArea = new JTextArea("HTML code?");
+        textArea.setName("TextArea");
+        textArea.setEnabled(false);
+        add(textArea);
     }
 }
