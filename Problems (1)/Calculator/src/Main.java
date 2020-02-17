06620222141127284a0adc5bb3b94f31c5cf4721
Grig Alex <alexengrigdev@gmail.com>
2020-02-17 19:02:41 +0300

    Add "Calculator" problem solution

---
+++ Problems (1)/Calculator/src/Main.java
@@ -0,0 +1,9 @@
+Calculator anonymousCalculator = new Calculator() {
+    public long sum(long val1, long val2) {
+        return val1 + val2;
+    }
+
+    public long subtraction(long val1, long val2) {
+        return val1 - val2;
+    }
+};
